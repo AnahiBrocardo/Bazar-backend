ca4119bbe1a72d1f6ed64371afa2b5f37c7ac871
Anahi Brocardo <anahibrocardo213@gmail.com>
2025-03-19 20:45:44 -0300

    Declaracion de metodos de IVentaService

--- src/main/java/com/bazar/api/service/IVentaService.java
+++ src/main/java/com/bazar/api/service/IVentaService.java
@@ -1,4 +1,23 @@
 package com.bazar.api.service;
 
+import com.bazar.api.dto.ApiRespuesta;
+import com.bazar.api.dto.DetalleVentaDTO;
+import com.bazar.api.dto.ResumenVentasDTO;
+import com.bazar.api.model.Estado;
+import com.bazar.api.model.Producto;
+import com.bazar.api.model.Venta;
+
+import java.time.LocalDate;
+import java.util.List;
+
 public interface IVentaService {
+   public ApiRespuesta<Venta> crearVenta(Venta venta);
+   public ApiRespuesta<Venta> obtenerVenta(Long id_venta);
+   public ApiRespuesta<Venta> actualizarVenta(Long id_venta, Venta venta);
+   public ApiRespuesta<Venta> cambiarEstadoVenta(Long id_venta, Estado estado);
+   public ApiRespuesta<List<Venta>> obtenerTodasVentas();
+   public ApiRespuesta<List<Producto>> obtenerProductosDeVenta (Long id_venta);
+   public ResumenVentasDTO obtenerResumenVentasPorFecha(LocalDate fecha);
+   public DetalleVentaDTO obtenerDetalleDeVentaDeMayorMonto();
+
 }
